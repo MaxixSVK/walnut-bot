a671032fdd36dd002f762cd337bb2f99dbe9982d
MaxixSVK <serdelmarek1@gmail.com>
2023-11-20 16:11:06 +0100

    update

--- src/commands/fun/randomGif.js
+++ src/commands/fun/randomGif.js
@@ -8,18 +8,17 @@ module.exports = {
     async execute(interaction) {
         const random = (arr) => arr[Math.floor(Math.random() * arr.length)];
         const images = [
-            'https://logos-world.net/wp-content/uploads/2020/12/Discord-Emblem.png',
-            'https://wallpaperaccess.com/full/765574.jpg',
-            'https://i0.wp.com/discordtemplates.me/icon.png',
-            'https://explodingstarpodcast.files.wordpress.com/2021/07/iconfinder_social-media_discord-alt_2417764.png',
+            'https://media.tenor.com/NzGpGWRN7MwAAAAC/nishikigi-chisato.gif',
+            'https://media.tenor.com/W3wUoMhulrwAAAAC/lycoris-recoil-lycoris.gif',
+            'https://media.tenor.com/s7MbaG16nDIAAAAC/lycoris-recoil-%E3%83%AA%E3%82%B3%E3%83%AA%E3%82%B9%E3%83%AA%E3%82%B3%E3%82%A4%E3%83%AB.gif',
+            'https://media.tenor.com/zEKmG9Tc0C0AAAAC/lycoris-recoil.gif',
+            'https://media.tenor.com/I4agzayzaXIAAAAC/lycoris-recoil-chisato.gif',
+            'https://media.tenor.com/LllLP-d1YfAAAAAd/lycoris-recoil.gif',
+            'https://media.tenor.com/2ysCn6BCcAsAAAAC/lycoris-recoil.gif',
+            'https://media.tenor.com/UXEHyHzR2moAAAAC/lycoris-recoil-nishikigi-chisato.gif',
+            'https://media.tenor.com/SCo7LfX56YEAAAAC/lycoris-recoil.gif',
         ];
 
-
-        const embed = new EmbedBuilder()
-            .setColor(config.Color)
-            .setTitle("Mini bot")
-            .setImage([random(images.toString())])
-
-        await interaction.reply({ embeds: [embed] });
+        await interaction.reply([random(images)].toString());
     },
 };

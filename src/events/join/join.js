fd7ae94f2dbf77a05ec8fe8b6a1b07b7fc4938dd
MaxixSVK <serdelmarek1@gmail.com>
2023-12-10 15:08:31 +0100

    update

--- src/events/join/join.js
+++ src/events/join/join.js
@@ -4,6 +4,16 @@ const config = require("../../config.json");
 module.exports = {
     name: Events.GuildMemberAdd,
     async execute(member) {
+        const random = (arr) => arr[Math.floor(Math.random() * arr.length)];
+        const images = [
+            'https://i.imgur.com/Oatpx2O.png',
+            'https://i.imgur.com/cWvOC3j.png',
+            'https://i.imgur.com/HpJtbGy.png',
+            'https://i.imgur.com/VR4Gsnp.png',
+            'https://i.imgur.com/TAQjRHu.png',
+            'https://i.imgur.com/NcgX2KS.png',
+        ];
+
         const members = member.guild.memberCount
         const guild = member.guild
         const verifyRole = guild.roles.cache.find(role => role.name === config.UnverifiedRole);
@@ -11,14 +21,14 @@ module.exports = {
         await member.roles.add(verifyRole);
 
         const welcomeEmbed = new EmbedBuilder()
-        .setTitle('Welcome to Café LycoReco!')
-        .setDescription(`Please make sure to read our <#990527996043603971> page and for everything you need to know about our server, and gain access to our chats once you are ready!`)
-        .setColor(config.Color)
-        .setImage('https://i.postimg.cc/4xBz7zHx/lycorisrecoil-001.gif')
-        .setFooter({ text: `Customer Number: ${members}`});
+            .setTitle('Welcome to Café LycoReco!')
+            .setDescription(`Please make sure to read our <#990527996043603971> page and for everything you need to know about our server, and gain access to our chats once you are ready!`)
+            .setColor(config.Color)
+            .setImage([random(images)].toString())
+            .setFooter({ text: `Customer Number: ${members}` });
 
         const channel = member.guild.channels.cache.find(channel => channel.name == config.WelcomeChannel);
-        channel.send({ content: `Hello ${member}! May we take your order?`, embeds: [welcomeEmbed]});
+        channel.send({ content: `Hello ${member}! May we take your order?`, embeds: [welcomeEmbed] });
 
     }
 }

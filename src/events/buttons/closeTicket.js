73b65336a174e2b0d621b2c02bdea9ac481dded8
MaxixSVK <serdelmarek1@gmail.com>
2023-12-15 19:42:05 +0100

    update

--- src/events/buttons/closeTicket.js
+++ src/events/buttons/closeTicket.js
@@ -5,51 +5,63 @@ module.exports = {
     async execute(interaction) {
         if (!interaction.isButton()) return;
         if (interaction.customId === 'closeTicket') {
-            const guild = interaction.guild;
-            const member = guild.members.cache.get(interaction.user.id);
-            const staffRoleID = interaction.client.config.staffRole;
-            const walnutID = interaction.client.user.id;
+            const guildId = interaction.guild.id
+            const configSchema = interaction.client.configSchema
 
-            if (member.roles.cache.has(staffRoleID)) {
+            const configSchemaData = await configSchema.find({
+                guildId: guildId
+            });
 
-                interaction.channel.permissionOverwrites.set([
-                    {
-                        id: interaction.guild.id,
-                        deny: [PermissionsBitField.Flags.ViewChannel],
-                    },
-                    {
-                        id: staffRoleID,
-                        allow: [PermissionsBitField.Flags.ViewChannel],
-                    },
-                    {
-                        id: walnutID,
-                        allow: [PermissionsBitField.Flags.ViewChannel],
-                    },
-                ]);
+            if (!configSchemaData.length == 0) {
+                const staffRoleId = configSchemaData.map(item => item.staffRoleId).toString()
+                const guild = interaction.guild;
+                const member = guild.members.cache.get(interaction.user.id);
+                const walnutID = interaction.client.user.id;
 
-                const deleteEmbed = new EmbedBuilder()
-                    .setTitle('Ticket has been closed')
-                    .setDescription('Now only staff team can see this ticket')
-                    .setColor('Red')
+                if (member.roles.cache.has(staffRoleId)) {
 
-                const deleteButton = new ActionRowBuilder()
-                    .addComponents(
-                        new ButtonBuilder()
-                            .setCustomId('transcriptAndDeletetTicket')
-                            .setLabel('Make a Transcript and Delete Ticket')
-                            .setEmoji('✅')
-                            .setStyle(ButtonStyle.Primary),
-                    )
+                    interaction.channel.permissionOverwrites.set([
+                        {
+                            id: interaction.guild.id,
+                            deny: [PermissionsBitField.Flags.ViewChannel],
+                        },
+                        {
+                            id: staffRoleId,
+                            allow: [PermissionsBitField.Flags.ViewChannel],
+                        },
+                        {
+                            id: walnutID,
+                            allow: [PermissionsBitField.Flags.ViewChannel],
+                        },
+                    ]);
 
-                await interaction.reply({ embeds: [deleteEmbed], components: [deleteButton] })
+                    const deleteEmbed = new EmbedBuilder()
+                        .setTitle('Ticket has been closed')
+                        .setDescription('Now only staff team can see this ticket')
+                        .setColor('Red')
+
+                    const deleteButton = new ActionRowBuilder()
+                        .addComponents(
+                            new ButtonBuilder()
+                                .setCustomId('transcriptAndDeletetTicket')
+                                .setLabel('Make a Transcript and Delete Ticket')
+                                .setEmoji('✅')
+                                .setStyle(ButtonStyle.Primary),
+                        )
+
+                    await interaction.reply({ embeds: [deleteEmbed], components: [deleteButton] })
+                }
+                else {
+                    const permsEmbed = new EmbedBuilder()
+                        .setTitle('No permissions')
+                        .setDescription('You don\'t have permission to close this ticket')
+                        .setColor('Red')
+
+                    await interaction.reply({ embeds: [permsEmbed], ephemeral: true })
+                }
             }
             else {
-                const permsEmbed = new EmbedBuilder()
-                    .setTitle('No permissions')
-                    .setDescription('You don\'t have permission to close this ticket')
-                    .setColor('Red')
-
-                await interaction.reply({ embeds: [permsEmbed], ephemeral: true })
+                return interaction.reply({ content: 'Please complete channel setup first', ephemeral: true });
             }
         }
     }
--- src/events/modals/ticketModal.js
+++ src/events/modals/ticketModal.js
@@ -6,67 +6,80 @@ module.exports = {
     async execute(interaction) {
         if (!interaction.isModalSubmit()) return;
         if (interaction.customId === 'ticketModal') {
-            const staffRoleID = interaction.client.config.staffRole;
-            const walnutID = interaction.client.user.id;
-            const ticketId = uniqid();
-            const channelName = `ticket-${ticketId}`
+            const guildId = interaction.guild.id
+            const configSchema = interaction.client.configSchema
 
-            interaction.guild.channels.create({
-                name: channelName,
-                type: ChannelType.GuildText,
-                permissionOverwrites: [
-                    {
-                        id: interaction.guild.id,
-                        deny: [PermissionsBitField.Flags.ViewChannel],
-                    },
-                    {
-                        id: interaction.user.id,
-                        allow: [PermissionsBitField.Flags.ViewChannel],
-                    },
-                    {
-                        id: staffRoleID,
-                        allow: [PermissionsBitField.Flags.ViewChannel],
-                    },
-                    {
-                        id: walnutID,
-                        allow: [PermissionsBitField.Flags.ViewChannel],
-                    },
-                ],
-            })
+            const configSchemaData = await configSchema.find({
+                guildId: guildId
+            });
 
-            const titleImput = interaction.fields.getTextInputValue('titTicketleInput');
-            const dscImput = interaction.fields.getTextInputValue('dscTicketInput');
+            if (!configSchemaData.length == 0) {
+                const staffRoleId = configSchemaData.map(item => item.staffRoleId).toString()
+                const walnutID = interaction.client.user.id;
+                const ticketId = uniqid();
+                const channelName = `ticket-${ticketId}`
+
+                interaction.guild.channels.create({
+                    name: channelName,
+                    type: ChannelType.GuildText,
+                    permissionOverwrites: [
+                        {
+                            id: interaction.guild.id,
+                            deny: [PermissionsBitField.Flags.ViewChannel],
+                        },
+                        {
+                            id: interaction.user.id,
+                            allow: [PermissionsBitField.Flags.ViewChannel],
+                        },
+                        {
+                            id: staffRoleId,
+                            allow: [PermissionsBitField.Flags.ViewChannel],
+                        },
+                        {
+                            id: walnutID,
+                            allow: [PermissionsBitField.Flags.ViewChannel],
+                        },
+                    ],
+                })
+
+                const titleImput = interaction.fields.getTextInputValue('titTicketleInput');
+                const dscImput = interaction.fields.getTextInputValue('dscTicketInput');
 
 
-            const ticketCreated = new EmbedBuilder()
-                .setTitle('Your ticket has been created')
-                .setDescription('Staff team will get in touch with you soon.')
-                .setColor('Green')
+                const ticketCreated = new EmbedBuilder()
+                    .setTitle('Your ticket has been created')
+                    .setDescription('Staff team will get in touch with you soon.')
+                    .setColor('Green')
 
-            const ticketEmbed = new EmbedBuilder()
-                .setTitle('The ticket has been created')
-                .setColor(interaction.client.config.color)
-                .addFields(
-                    { name: 'Ticket\'s name:', value: titleImput, inline: false },
-                    { name: 'Ticket\'s description:', value: dscImput, inline: false },
-                    { name: 'Ticket ID:', value: ticketId, inline: false },
-                    
-                )
+                const ticketEmbed = new EmbedBuilder()
+                    .setTitle('The ticket has been created')
+                    .setColor(interaction.client.config.color)
+                    .addFields(
+                        { name: 'Ticket\'s name:', value: titleImput, inline: false },
+                        { name: 'Ticket\'s description:', value: dscImput, inline: false },
+                        { name: 'Ticket ID:', value: ticketId, inline: false },
 
-            setTimeout(() => {
-                const button = new ActionRowBuilder()
-                .addComponents(
-                    new ButtonBuilder()
-                        .setCustomId('closeTicket')
-                        .setLabel('Close ticket')
-                        .setEmoji('✅')
-                        .setStyle(ButtonStyle.Primary),
-                )
-                const ticketChannel = interaction.guild.channels.cache.find(channel => channel.name == channelName);
-                ticketChannel.send({ content: `<@${interaction.user.id}> has created ticket <@&${staffRoleID}>`, embeds: [ticketEmbed], components: [button] });
-            }, 3000);
+                    )
 
-            interaction.reply({ embeds: [ticketCreated], ephemeral: true });
+                setTimeout(() => {
+                    const button = new ActionRowBuilder()
+                        .addComponents(
+                            new ButtonBuilder()
+                                .setCustomId('closeTicket')
+                                .setLabel('Close ticket')
+                                .setEmoji('✅')
+                                .setStyle(ButtonStyle.Primary),
+                        )
+                    const ticketChannel = interaction.guild.channels.cache.find(channel => channel.name == channelName);
+                    ticketChannel.send({ content: `<@${interaction.user.id}> has created ticket <@&${staffRoleId}>`, embeds: [ticketEmbed], components: [button] });
+                }, 3000);
+
+                interaction.reply({ embeds: [ticketCreated], ephemeral: true });
+            }
+            else {
+                return interaction.reply({ content: 'Please complete setup first', ephemeral: true });
+            }
+
         }
     }
 }

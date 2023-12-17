8640f4304d66042b4e1a6914118c311519852762
MaxixSVK <serdelmarek1@gmail.com>
2023-12-17 12:18:45 +0100

    fixing bad code

--- src/events/modals/ticketModal.js
+++ src/events/modals/ticketModal.js
@@ -4,83 +4,82 @@ const uniqid = require('uniqid');
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
-        if (!interaction.isModalSubmit()) return;
-        if (interaction.customId === 'ticketModal') {
-            const guildId = interaction.guild.id
-            const configSchema = interaction.client.configSchema
+        if (!interaction.isModalSubmit() || interaction.customId !== 'ticketModal') return;
+        const configSchema = interaction.client.configSchema
+        const guildId = interaction.guild.id
 
-            const configSchemaData = await configSchema.find({
-                guildId: guildId
-            });
+        const configSchemaData = await configSchema.find({
+            guildId: guildId
+        });
 
-            if (!configSchemaData.length == 0) {
-                const staffRoleId = configSchemaData.map(item => item.staffRoleId).toString()
-                const walnutID = interaction.client.user.id;
-                const ticketId = uniqid();
-                const channelName = `ticket-${ticketId}`
+        if (!configSchemaData.length) {
+            const errorEmbed = new EmbedBuilder()
+                .setColor('Red')
+                .setTitle('Error')
+                .setDescription('Config setup has not been completed, please contact server administrator')
 
-                interaction.guild.channels.create({
-                    name: channelName,
-                    type: ChannelType.GuildText,
-                    permissionOverwrites: [
-                        {
-                            id: interaction.guild.id,
-                            deny: [PermissionsBitField.Flags.ViewChannel],
-                        },
-                        {
-                            id: interaction.user.id,
-                            allow: [PermissionsBitField.Flags.ViewChannel],
-                        },
-                        {
-                            id: staffRoleId,
-                            allow: [PermissionsBitField.Flags.ViewChannel],
-                        },
-                        {
-                            id: walnutID,
-                            allow: [PermissionsBitField.Flags.ViewChannel],
-                        },
-                    ],
-                })
-
-                const titleImput = interaction.fields.getTextInputValue('titTicketleInput');
-                const dscImput = interaction.fields.getTextInputValue('dscTicketInput');
-
-
-                const ticketCreated = new EmbedBuilder()
-                    .setTitle('Your ticket has been created')
-                    .setDescription('Staff team will get in touch with you soon.')
-                    .setColor('Green')
-
-                const ticketEmbed = new EmbedBuilder()
-                    .setTitle('The ticket has been created')
-                    .setColor(interaction.client.config.color)
-                    .addFields(
-                        { name: 'Ticket\'s name:', value: titleImput, inline: false },
-                        { name: 'Ticket\'s description:', value: dscImput, inline: false },
-                        { name: 'Ticket ID:', value: ticketId, inline: false },
-
-                    )
-
-                setTimeout(() => {
-                    const button = new ActionRowBuilder()
-                        .addComponents(
-                            new ButtonBuilder()
-                                .setCustomId('closeTicket')
-                                .setLabel('Close ticket')
-                                .setEmoji('✅')
-                                .setStyle(ButtonStyle.Primary),
-                        )
-                    const ticketChannel = interaction.guild.channels.cache.find(channel => channel.name == channelName);
-                    ticketChannel.send({ content: `<@${interaction.user.id}> has created ticket <@&${staffRoleId}>`, embeds: [ticketEmbed], components: [button] });
-                }, 3000);
-
-                interaction.reply({ embeds: [ticketCreated], ephemeral: true });
-            }
-            else {
-                return interaction.reply({ content: 'Please complete setup first', ephemeral: true });
-            }
-
+            return interaction.reply({ embeds: [errorEmbed], ephemeral: true });
         }
+
+        const staffRoleId = configSchemaData.map(item => item.staffRoleId).toString()
+        const walnutID = interaction.client.user.id;
+        const ticketId = uniqid();
+        const channelName = `ticket-${ticketId}`
+
+        interaction.guild.channels.create({
+            name: channelName,
+            type: ChannelType.GuildText,
+            permissionOverwrites: [
+                {
+                    id: interaction.guild.id,
+                    deny: [PermissionsBitField.Flags.ViewChannel],
+                },
+                {
+                    id: interaction.user.id,
+                    allow: [PermissionsBitField.Flags.ViewChannel],
+                },
+                {
+                    id: staffRoleId,
+                    allow: [PermissionsBitField.Flags.ViewChannel],
+                },
+                {
+                    id: walnutID,
+                    allow: [PermissionsBitField.Flags.ViewChannel],
+                },
+            ],
+        })
+
+        const titleImput = interaction.fields.getTextInputValue('titTicketleInput');
+        const dscImput = interaction.fields.getTextInputValue('dscTicketInput');
+
+        const ticketCreated = new EmbedBuilder()
+            .setTitle('Your ticket has been created')
+            .setDescription('Staff team will get in touch with you soon.')
+            .setColor('Green')
+
+        const ticketEmbed = new EmbedBuilder()
+            .setTitle('The ticket has been created')
+            .setColor(interaction.client.config.color)
+            .addFields(
+                { name: 'Ticket\'s name:', value: titleImput, inline: false },
+                { name: 'Ticket\'s description:', value: dscImput, inline: false },
+                { name: 'Ticket ID:', value: ticketId, inline: false },
+
+            )
+
+        setTimeout(() => {
+            const button = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('closeTicket')
+                        .setLabel('Close ticket')
+                        .setEmoji('✅')
+                        .setStyle(ButtonStyle.Primary),
+                )
+            const ticketChannel = interaction.guild.channels.cache.find(channel => channel.name == channelName);
+            ticketChannel.send({ content: `<@${interaction.user.id}> has created ticket <@&${staffRoleId}>`, embeds: [ticketEmbed], components: [button] });
+        }, 3000);
+
+        interaction.reply({ embeds: [ticketCreated], ephemeral: true });
     }
 }
-
--- src/events/modals/verifyModal.js
+++ src/events/modals/verifyModal.js
@@ -3,50 +3,57 @@ const { Events, EmbedBuilder } = require('discord.js');
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
-        if (!interaction.isModalSubmit()) return;
-        if (interaction.customId === 'captchaModal') {
-            const verifySchema = interaction.client.verifySchema
+        if (!interaction.isModalSubmit() || interaction.customId !== 'captchaModal') return;
+        const verifySchema = interaction.client.verifySchema
+        const guildId = interaction.guild.id
 
-            const userImput = interaction.fields.getTextInputValue('captchaInput').toUpperCase();
+        const verifySchemaData = await verifySchema.find({
+            guildId: guildId,
+            id: interaction.user.id
+        });
 
-            const data = await verifySchema.find({
-                id: interaction.user.id
-            });
+        const userImput = interaction.fields.getTextInputValue('captchaInput').toUpperCase();
+        const captcha = verifySchemaData.map(item => item.captcha).toString()
 
-            let captcha = data.map(item => item.captcha).toString()
+        if (userImput !== captcha) {
+            const noverifyEmbed = new EmbedBuilder()
+                .setColor('Red')
+                .setTitle('Verification was not completed')
+                .setDescription('You entered wrong captcha please try again.')
 
-            if (userImput == captcha) {
-                const verifyEmbed = new EmbedBuilder()
-                    .setColor('Green')
-                    .setTitle('Verification completed')
-                    .setDescription('Enjoy your time on a server')
-
-                const configSchema = interaction.client.configSchema
-                const guildId = interaction.guild.id
-
-                const configSchemaData = await configSchema.find({
-                    guildId: guildId
-                });
-
-                const verifyRole = configSchemaData.map(item => item.unverifiedRoleId).toString()
-                const member = interaction.member;
-                const memberId = interaction.user.id;
-
-                await verifySchema.deleteMany({ 
-                    guildId: guildId,
-                    id: memberId 
-                })
-                await member.roles.remove(verifyRole);
-                interaction.reply({ embeds: [verifyEmbed], ephemeral: true })
-            }
-            else {
-                const noverifyEmbed = new EmbedBuilder()
-                    .setColor('Red')
-                    .setTitle('Verification was not completed')
-                    .setDescription('You entered wrong captcha please try again.')
-
-                interaction.reply({ embeds: [noverifyEmbed], ephemeral: true })
-            }
+            return interaction.reply({ embeds: [noverifyEmbed], ephemeral: true })
         }
+
+        const configSchema = interaction.client.configSchema
+        const configSchemaData = await configSchema.find({
+            guildId: guildId
+        });
+
+        if (!configSchemaData.length) {
+            const errorEmbed = new EmbedBuilder()
+                .setColor('Red')
+                .setTitle('Error')
+                .setDescription('Config setup has not been completed, please contact server administrator')
+
+            return interaction.reply({ embeds: [errorEmbed], ephemeral: true });
+        }
+
+        const verifyRole = configSchemaData.map(item => item.unverifiedRoleId).toString()
+        const member = interaction.member;
+        const memberId = interaction.user.id;
+
+        await verifySchema.deleteMany({
+            guildId: guildId,
+            id: memberId
+        })
+
+        await member.roles.remove(verifyRole);
+
+        const verifyEmbed = new EmbedBuilder()
+            .setColor('Green')
+            .setTitle('Verification completed')
+            .setDescription('Enjoy your time on a server')
+
+        interaction.reply({ embeds: [verifyEmbed], ephemeral: true })
     }
 }

a41865203783a20231b4019cf9a38848b8642e7b
MaxixSVK <serdelmarek1@gmail.com>
2023-12-19 17:24:46 +0100

    Usage of API routes

---
+++ src/api/routes/serverConfig.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+module.exports = (adminToken, client) => {
+    const router = express.Router();
+
+    router.get('/serverconfig/:serverId', async (req, res) => {
+        const authToken = req.headers['authorization'];
+
+        if (!authToken || authToken !== adminToken) {
+            return res.status(403).send({
+                message: 'Forbidden. Invalid token.',
+            });
+        }
+
+        const serverId = req.params.serverId;
+        const configSchema = client.configSchema
+
+        const configSchemaData = await configSchema.find({
+            guildId: serverId.slice(1)
+        });
+
+        if (!configSchemaData.length) {
+            return res.status(404).send({
+                message: 'Config not found.',
+            });
+        }
+        res.status(200).send({
+            message: 'Config found.',
+            ...configSchemaData,
+        });
+    });
+
+    return router;
+}

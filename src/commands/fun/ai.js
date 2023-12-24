a4be2d8c2fc8ed82fa1025284d88e5e3bc4e2598
MaxixSVK <serdelmarek1@gmail.com>
2023-12-24 16:58:12 +0100

    Add chat functionality with Walnut AI

---
+++ src/commands/fun/ai.js
@@ -0,0 +1,48 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { OpenAI } = require('openai');
+require('dotenv').config()
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('chat')
+        .setDescription('Chat with Walnut')
+        .addStringOption(option => option
+            .setName('text')
+            .setDescription('What you want to say to Walnut')
+            .setRequired(true)),
+    async execute(interaction) {
+        if (interaction.user.id !== '694569759093817374') { 
+            const noPermsEmbed = new EmbedBuilder()
+                .setTitle('No Permission')
+                .setDescription('You do not have permission to use this command.')
+                .setColor('Red')
+            return interaction.reply({ embeds: [noPermsEmbed], ephemeral: true}); }
+
+        const prompt = interaction.options.getString('text');
+
+        const openai = new OpenAI({ apiKey: process.env.openAiToken });
+
+        const response = await openai.completions.create({
+            model: 'gpt-3.5-turbo-instruct',
+            prompt: `I am friendly discord bot Walnut and user is chating with you: ${prompt}`,
+            temperature: 0,
+            max_tokens: 100,
+            top_p: 1,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,   
+        });
+
+        const answer = response.choices[0].text;
+        try {
+            interaction.reply(answer);
+        }
+        catch (error) {
+            const errorEmbed = new EmbedBuilder()
+                .setTitle('Error')
+                .setDescription('There was an error with your request\nSorry for the inconvenience')
+                .setColor('Red')
+
+                interaction.reply({ embeds: [errorEmbed], ephemeral: true });
+        }
+    },
+};

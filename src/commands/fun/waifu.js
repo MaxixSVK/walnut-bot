6466d3423fb7a9b83a9e90ec32abb0dabc99602a
MaxixSVK <serdelmarek1@gmail.com>
2023-12-28 11:13:35 +0100

    Add waifu command implementation

---
+++ src/commands/fun/waifu.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('waifu')
+        .setDescription('Get a random waifu picture'),
+    async execute(interaction) {
+        if (interaction.user.id !== '694569759093817374') {
+            const noPermsEmbed = new EmbedBuilder()
+                .setTitle('No Permission')
+                .setDescription('You do not have permission to use this command.')
+                .setColor('Red')
+            return interaction.reply({ embeds: [noPermsEmbed], ephemeral: true });
+        }
+        try {
+            const response = await axios.get('https://api.waifu.pics/sfw/waifu');
+            const imageUrl = response.data.url;
+
+            await interaction.reply(imageUrl);
+        } catch (error) {
+            const errorEmbed = new EmbedBuilder()
+                .setTitle('Error')
+                .setDescription('There was an error with your request\nSorry for the inconvenience')
+                .setColor('Red')
+
+            await interaction.reply({ embeds: [errorEmbed], ephemeral: true });
+            console.error(error);
+        }
+    },
+};

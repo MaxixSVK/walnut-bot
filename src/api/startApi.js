5673434182154418dda8bff7c15ff5e310e5a214
MaxixSVK <dev@maxix.sk>
2025-03-03 17:08:20 +0100

    Update API to serve health check responses only

--- src/api/startApi.js
+++ src/api/startApi.js
@@ -1,13 +1,21 @@
+/*
+ * --------------------------------------------------------------------------------------------
+ * Due to time constraints, this API will not be used as originally anticipated.
+ * Therefore, I will not implement any endpoints here; it will only be used for health checks.
+ * --------------------------------------------------------------------------------------------
+ */
+
 const express = require('express');
 
 module.exports = function() {
     const app = express();
     const port = 4300;
+    const package = require('../../package.json');
 
     app.use(express.json());
 
     app.get('/', (req, res) => {
-        res.status(200).send({ message: 'Hello, world! This API is in pre-aplha version.' });
+        res.status(200).send({ name: package.name, version: package.version });
     });
 
     app.get('/status', (req, res) => {
